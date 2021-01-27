04e8ef117ad74039134e06008d463e69b1a5fd12
Sergey Shvets <justone128@gmail.com>
2021-01-27 16:14:18 +0200

    Added basic theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 
-import './index.css';
 import App from './App';
+import theme from './constants/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
