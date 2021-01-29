b91cdf9d6729794df0206b4d0f4cd81337c95407
Sergey Shvets <justone128@gmail.com>
2021-01-29 02:01:03 +0200

    Refactoring useBookIcon

--- src/hooks/useBookIcon.ts
+++ src/hooks/useBookIcon.ts
@@ -1,30 +1,39 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, MutableRefObject } from 'react'
 
 type TUpdateIcon = (event?: React.MouseEvent<HTMLButtonElement>) => void
+type TGetIndex = (array: Array<string>) => number
 
 const useBookIcon = (): [string, TUpdateIcon] => {
+  const randomizeIndex: TGetIndex = (array) => Math.floor(Math.random() * array.length)
+
+  const updateIcon = (
+    getIndex: TGetIndex,
+    array: Array<string>,
+    ref: MutableRefObject<number | null>,
+    setIcon: React.Dispatch<React.SetStateAction<string>>,
+  ) => () => {
+    let newIndex: number = getIndex(array)
+
+    while (ref.current === newIndex) {
+      newIndex = getIndex(array)
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    ref.current = newIndex
+
+    setIcon(array[newIndex])
+  }
+
   const titleIcons = ['📘', '📗', '📕', '📔', '📓', '📙', '📚', '📖']
+  const randomIndex = randomizeIndex(titleIcons)
+  const currentIndexIconRef = useRef<null | number>(null)
 
-  const randomizeIndex = () => Math.floor(Math.random() * titleIcons.length)
-
-  const currentIndexIconRef = useRef(randomizeIndex())
+  currentIndexIconRef.current = randomIndex
 
   const currentIcon = titleIcons[currentIndexIconRef.current]
   const [icon, setIcon] = useState(currentIcon)
 
-  const updateIcon = () => {
-    let newIndex: number = randomizeIndex()
-
-    while (currentIndexIconRef.current === newIndex) {
-      newIndex = randomizeIndex()
-    }
-
-    currentIndexIconRef.current = newIndex
-
-    setIcon(titleIcons[newIndex])
-  }
-
-  return [icon, updateIcon]
+  return [icon, updateIcon(randomizeIndex, titleIcons, currentIndexIconRef, setIcon)]
 }
 
 export default useBookIcon
